752ac6fcea7b317fedb0d1ae5b193e73d50ceca8
dmitry040698 <dima.melkunas@mail.ru>
2021-07-24 02:01:11 +0300

    Add new file with one limitation

---
+++ task_3.cs
@@ -0,0 +1,22 @@
+using System;
+
+class task_3
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 3){
+            Console.WriteLine("Input 3 or more arguments!\nExample: rock paper scissors");
+            return;
+        }
+        
+        /*
+        Console.WriteLine(args.Length);
+        foreach (string ar in args) {
+            Console.WriteLine(ar);
+        }
+        */
+
+
+
+    }
+}
